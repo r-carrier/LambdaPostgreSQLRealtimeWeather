fe8a71e154450976c637651c65b8b165cf5de812
r-carrier <robertjaycarrier@gmail.com>
2024-06-24 23:28:36 -0400

    Added a few new columns to the realtime_weather table and modified lambda_function.py accordingly
    Added tomorrow.io API information to AWS Secrets Manager...could probably add the URL to my secret as well or perhaps I can add it as an environment variable later?

--- lambda_function.py
+++ lambda_function.py
@@ -3,31 +3,23 @@ from psycopg2 import sql
 import json
 import urllib3
 from botocore.config import Config
-from botocore.exceptions import ClientError
 import boto3
 
-secret_name = ''
-fields = {'apikey': '',
-          'location': '',
-          'units': 'imperial'}
+secret_name = 'fishing-secrets'
 insert_query = '''
     insert into fw1.realtime_weather
-    (request_date, location_name, temperature, cloud_cover)
-    VALUES (%s, %s, %s, %s);
+    (request_date, location_name, temperature, cloud_cover, precip_probability, rain_intensity, weather_code)
+    VALUES (%s, %s, %s, %s, %s, %s, %s);
     '''
 
 http = urllib3.PoolManager()
 
 
 def get_secret():
-    try:
-        print("trying to get secrets")
-        secret_client = boto3.client('secretsmanager')
-        secret_response = secret_client.get_secret_value(
-            SecretId=secret_name)
-        secret_dict = json.loads(secret_response['SecretString'])
-    except Exception:
-        print("There was an error loading the secret from AWS Secrets Manager:\n" + Exception)
+    secret_client = boto3.client('secretsmanager')
+    secret_response = secret_client.get_secret_value(
+        SecretId=secret_name)
+    secret_dict = json.loads(secret_response['SecretString'])
     return secret_dict
 
 
@@ -35,15 +27,17 @@ def connect_to_db(secret_dict):
     conn = psycopg2.connect(
         host=secret_dict['host'],
         port=secret_dict['port'],
-        dbname=secret_dict['dbname'],
+        dbname=secret_dict['db_name'],
         user=secret_dict['db_username'],
         password=secret_dict['db_password']
     )
-
     return conn
 
 
-def get_weather():
+def get_weather(secret_dict):
+    fields = {'apikey': secret_dict['tomorrow_io_apikey'],
+              'location': secret_dict['rt_location'],
+              'units': secret_dict['rt_units']}
     response = http.request('GET',
                             'https://api.tomorrow.io/v4/weather/realtime',
                             fields=fields)
@@ -55,19 +49,22 @@ def insert_into_db(conn, query, data):
     cur = conn.cursor()
     cur.execute(query, data)
     conn.commit()
-    print("executed insert statement...")
 
 
 def lambda_handler(event, context):
-    weather_data = get_weather()
-    request_time = weather_data['data']['time']
+    secret_dict = get_secret()
+
+    weather_data = get_weather(secret_dict)
+    request_time = 'NOW()'
     location_name = weather_data['location']['name']
     temperature = weather_data['data']['values']['temperature']
     cloud_cover = weather_data['data']['values']['cloudCover']
-    print(cloud_cover)
+    precip_probability = weather_data['data']['values']['precipitationProbability']
+    rain_intensity = weather_data['data']['values']['precipitationProbability']
+    weather_code = weather_data['data']['values']['weatherCode']
 
-    insert_query_data = (request_time, location_name, temperature, cloud_cover)
-    secret_dict = get_secret()
+    insert_query_data = (request_time, location_name, temperature, cloud_cover, precip_probability, rain_intensity, weather_code)
+
     db_connection = connect_to_db(secret_dict)
     insert_into_db(db_connection, insert_query, insert_query_data)
     db_connection.close()
